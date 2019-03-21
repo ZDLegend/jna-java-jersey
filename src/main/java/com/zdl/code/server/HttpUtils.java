280e015b46d12c5128f0ef828fce19cae1bb1038
张明浩 <463764941@qq.com>
2019-03-21 17:49:41 +0800

    引入java11新特性：httpClient异步方式

--- src/main/java/com/zdl/code/server/HttpUtils.java
+++ src/main/java/com/zdl/code/server/HttpUtils.java
@@ -1,60 +1,44 @@
 package com.zdl.code.server;
 
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpConnectionManager;
-import org.apache.commons.httpclient.SimpleHttpConnectionManager;
-import org.apache.commons.httpclient.methods.InputStreamRequestEntity;
-import org.apache.commons.httpclient.methods.PostMethod;
-import org.apache.commons.httpclient.params.HttpConnectionManagerParams;
+import org.glassfish.grizzly.http.util.HttpStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 
 /**
  * Created by ZDLegend on 2016/12/21.
  */
 public class HttpUtils {
 
+    private static Logger logger = LoggerFactory.getLogger(HttpUtils.class);
+
     private static final int CONNECTION_TIME_OUT = 30000;
 
-    public static int sendAlarm(String url, String data) {
+    public static void sendAlarm(String url, String data) {
 
-        HttpConnectionManager connectionManager = new SimpleHttpConnectionManager(false);
-        HttpConnectionManagerParams connectionParams = new HttpConnectionManagerParams();
-        connectionParams.setConnectionTimeout(CONNECTION_TIME_OUT);
+        java.net.http.HttpClient client = java.net.http.HttpClient.newHttpClient();
 
-        connectionManager.setParams(connectionParams);
-        HttpClient client = new HttpClient(connectionManager);
-        InputStream inputStream = new ByteArrayInputStream(data.getBytes());
+        var r = HttpRequest.newBuilder(URI.create(url))
+                .timeout(Duration.ofMillis(CONNECTION_TIME_OUT))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(data))
+                .build();
 
-        PostMethod httpMethod = new PostMethod(url);
-
-        int status = 200;
-        try {
-            InputStreamRequestEntity inputStreamEntity = new InputStreamRequestEntity(inputStream);
-
-            httpMethod.setRequestEntity(inputStreamEntity);
-            httpMethod.setRequestHeader("Connection", "Keep-Alive");
-            httpMethod.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-
-            client.executeMethod(httpMethod);
-            status = httpMethod.getStatusLine().getStatusCode();
-            httpMethod.releaseConnection();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-
-        return status;
-    }
-
-    public static int action(String s1, String s2) throws IOException {
-
-        int ret = sendAlarm(s1, s2);
-        if (ret == 200) {
-            return 0;
-        }
-
-        throw new IOException("url:" + s1 + " connected error, status:" + ret);
+        client.sendAsync(r, HttpResponse.BodyHandlers.ofString())
+                .whenComplete((request, throwable) -> {
+                    if (throwable != null) {
+                        logger.error(throwable.getMessage(), throwable);
+                    }
+                    var code = request.statusCode();
+                    if (code == HttpStatus.OK_200.getStatusCode()) {
+                        logger.info("http send success");
+                    } else {
+                        logger.error("http send fail, code:{}, body;{}", request.statusCode(), request.body());
+                    }
+                });
     }
 }
